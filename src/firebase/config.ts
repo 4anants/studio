77c65801e775f9e18eb8e64a6eff43dcf6e04c14
AnaNT ShAH <mr.anantshah@gmail.com>
2025-12-08 12:54:59 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,11 @@
 
 // This file is intentionally left blank. 
 // The firebaseConfig object will be populated by the Firebase service.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "filesafe2-22629644-c6897",
+  "appId": "1:729314575934:web:c9020b8f63f808f6b8336f",
+  "apiKey": "AIzaSyDUhgmvhznHlZGWFS5_T06RH8VqtUKjH8A",
+  "authDomain": "filesafe2-22629644-c6897.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "729314575934"
+};
